091db4580c778378d435a3d37e6af46fe675f0ae
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-02-27 04:05:20 +0100

    'chnages'

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import '../styles/Navbar.scss'
-import logo from '../images/logo.png'
+import React,{useState} from "react";
+import "../styles/Navbar.scss";
+import logo from "../images/logo.png";
 function NavBar() {
-  return (
-    <div className='navbar'>
-        <img src={logo} alt="my logo" />
-        <button className='humburger'>---</button>        
-    </div>
-  )
+  const [isOpen , setIsOpen] = useState(false)
+  const css = `
+  .hum::before{
+    transform: rotate(-45deg);
+}
+.hum::after{
+  transform: rotate(45deg);
 }
 
-export default NavBar
+
+  `
+  
+  return (
+    <div className="navbar">
+      <img src={logo} alt="my logo" />
+      <div className="cursor-menu"><button className="hum" onClick={() => setIsOpen(!isOpen)}></button></div>
+      
+      <div className="links-hum" style={{transform: isOpen && 'translateX(0)'}}>
+       
+       <a href="">
+          <button>About me</button>
+        </a>
+        <a href="">
+          <button>Projects</button>
+        </a>
+        <a href="">
+          <button>Conatct me</button>
+        </a>
+      </div>
+     {isOpen &&
+      <style>
+        {css}
+      </style>}
+    </div>
+  );
+}
+
+export default NavBar;

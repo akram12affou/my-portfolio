0d3a2e97d570880059c4d597c804ae90782846be
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-02-28 05:08:10 +0100

    'changes'

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import '../styles/Contact.scss'
+import React from "react";
+import "../styles/Contact.scss";
 function Contact() {
   return (
-    <div className='contact'>
-        <h2>Send me an email</h2>
-        Name  <input type="text" />
-        E-mail  <input type="email" />
-        Message  <textarea></textarea>
-    </div>
-  )
+    <>
+      <div className="contact">
+        <div> <h2>SEND ME AN EMAIL</h2></div>
+       
+        <div className="grid-flex">
+          <div className="grid">
+            <div className="inputs">
+              <div>Name <br /><input type="text" /></div>
+            <div>E-mail <br /><input type="email" /></div> 
+            </div>
+            <div className="textarea">
+              <p>Message </p>
+              <textarea></textarea>
+            </div>
+          </div>
+        </div>
+        <div><button>Send email</button></div>
+        
+      </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;

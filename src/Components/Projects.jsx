7cff8c1d4fe9680969e4e328bb29d8574e6d77e7
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-03-01 14:12:09 +0100

    'changes'

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,15 +1,15 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../styles/Projects.scss";
-import Links from './Links'
-import chat_app1 from '../images/chat-apk-1.png'
-import chat_app2 from '../images/chat-apk-2.png'
-import chat_app3 from '../images/chat-apk-3.png'
-import chat_app4 from '../images/chat-apk-4.png'
-import Carousel from 'react-bootstrap/Carousel';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Links from "./Links";
+import chat_app1 from "../images/chat-apk-1.png";
+import chat_app2 from "../images/chat-apk-2.png";
+import chat_app3 from "../images/chat-apk-3.png";
+import chat_app4 from "../images/chat-apk-4.png";
+import Carousel from "react-bootstrap/Carousel";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import "bootstrap/dist/css/bootstrap.min.css";
 function Projects() {
-  const [index , setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
   // const items = [
   //   {
   //     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
@@ -28,55 +28,124 @@ function Projects() {
   //   }
   // ];
   const handleClick = () => {
-    setIndex(index+1);
-  }
+    setIndex(index + 1);
+  };
   return (
     <>
-   
-    <div className="projects">
-    <h2>Portfolio</h2>
-      <span>
-        // these are my favorite projects I've worked on for the past year. Have
-        a look around and make sure to hit me up!
-      </span>
-      <div className="projects-container">
-        <div className="project">
-    
-          <button className="show-projects">
-            View Project
-          </button>
-          <Carousel>
-        
-      <Carousel.Item>
-      
-      <img src={chat_app1} alt="" />
-       
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={chat_app2} alt="" />
+      <div className="projects">
+        <h2>Portfolio</h2>
+        <span>
+          // these are my favorite projects I've worked on for the past year.
+          Have a look around and make sure to hit me up!
+        </span>
+        <div className="projects-container">
+          <div className="project">
+            <button className="show-projects">
+              <RemoveRedEyeIcon /> <div>VIEW PROJECT</div>
+            </button>
+            <Carousel>
+              <Carousel.Item>
+                <img src={chat_app1} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app2} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app3} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app4} alt="" />
 
-      
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={chat_app3} alt="" />
-
-        
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={chat_app4} alt="" />
-
-        {/* <Carousel.Caption>
+                {/* <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption> */}
-      </Carousel.Item>
-    </Carousel>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          <div className="project">
+            <button className="show-projects">
+              <RemoveRedEyeIcon /> <div>VIEW PROJECT</div>
+            </button>
+            <Carousel>
+              <Carousel.Item>
+                <img src={chat_app1} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app2} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app3} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app4} alt="" />
+
+                {/* <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          <div className="project">
+            <button className="show-projects">
+              <RemoveRedEyeIcon /> <div>VIEW PROJECT</div>
+            </button>
+            <Carousel>
+              <Carousel.Item>
+                <img src={chat_app1} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app2} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app3} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app4} alt="" />
+
+                {/* <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          <div className="project">
+            <button className="show-projects">
+              <RemoveRedEyeIcon /> <div>VIEW PROJECT</div>
+            </button>
+            <Carousel>
+              <Carousel.Item>
+                <img src={chat_app1} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app2} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app3} alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src={chat_app4} alt="" />
+
+                {/* <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption> */}
+              </Carousel.Item>
+            </Carousel>
+          </div>
         </div>
       </div>
-    </div>
-    <Links/>
+      <Links />
     </>
   );
 }
